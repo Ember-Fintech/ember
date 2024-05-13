b4be70c39ea507814e248ded634222d107cf46ca
Govinda Vashishtha <govindavashishtha@gmail.com>
2024-05-13 20:50:12 +0530

    icons

--- app/hooks/useLoggedIn.ts
+++ app/hooks/useLoggedIn.ts
@@ -1,7 +1,27 @@
-import { create } from 'zustand'
+import { create } from "zustand"
+import { persist, createJSONStorage } from "zustand/middleware"
 
-export const useLoggedIn = create((set) => ({
-  isLoggedIn: true,
-  changeLoggedInStatus: () => set((state) => ({ isLoggedIn: !state.isLoggedIn })),
-}))
+export const useLoggedIn = create(
+  persist(
+    (set) => ({
+      isLoggedIn: false,
+      changeLoggedInStatus: () => set((state) => ({ isLoggedIn: !state.isLoggedIn })),
+    }),
+    {
+      name: "logged-in-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+      onRehydrateStorage: (state) => {
+        console.log("hydration starts", state)
 
+        // optional
+        return (state, error) => {
+          if (error) {
+            console.log("an error happened during hydration", error)
+          } else {
+            console.log("hydration finished")
+          }
+        }
+      },
+    },
+  ),
+)
